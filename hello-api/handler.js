a6b64ad64e92cb2a3ad4c01cf176990e6a66017b
김찬민 <chanmin@gimchanmin-ui-MacBookPro.local>
2023-04-23 18:44:09 +0900

    chore: aws-lambda를 활용해 인자 타입을 지정한다

--- hello-api/handler.js
+++ hello-api/handler.js
@@ -1,18 +1,12 @@
 "use strict";
-
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v1.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+export const handler = async (event) => {
+    if (!event.queryStringParameters || !event.queryStringParameters.name) {
+        return { statusCode: 404, body: "Name not found" };
+    }
+    return {
+        statusCode: 200,
+        body: `Hello, ${event.queryStringParameters.name}}`,
+    };
+    // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+    // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
---
+++ hello-api/handler.ts
@@ -0,0 +1,16 @@
+"use strict";
+
+import { APIGatewayEvent } from "aws-lambda";
+
+export const handler = async (event: APIGatewayEvent) => {
+  if (!event.queryStringParameters || !event.queryStringParameters.name) {
+    return { statusCode: 404, body: "Name not found" };
+  }
+  return {
+    statusCode: 200,
+    body: `Hello, ${event.queryStringParameters.name}}`,
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
